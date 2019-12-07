e25791b18e60790bf2abfa119aa2b7b065db4d9b
extraname <zorraider@gmail.com>
2019-12-07 16:46:41 +0200

    1st

--- lesson0712.py
+++ lesson0712.py
@@ -1,3 +1,54 @@
-from flask import Flask
+from flask import Flask, Response, jsonify, request
 
 app = Flask(__name__)
+PEOPLE = [
+    {
+        'id': 0,
+        'name': "Steve",
+        'age': 20,
+        'country': 'US'
+    },
+    {
+        'id': 1,
+        'name': "Jack",
+        'age': 11,
+        'country': 'Spain'
+    },
+    {
+        'id': 2,
+        'name': "John",
+        'age': 30,
+        'country': 'Poland'
+    },
+    {
+        'id': 3,
+        'name': "Selena",
+        'age': 21,
+        'country': 'US'
+    }
+]
+
+
+@app.route('/version')
+def version():
+    return '1.0'
+
+
+@app.route('/people')
+def people():
+    country = request.args.get('country')
+    if country is not None:
+        return jsonify([p for p in PEOPLE if p["country"] == country])
+    return jsonify(PEOPLE)
+
+
+@app.route('/people/<id>')
+def get_person(id):
+    try:
+        return jsonify(PEOPLE[int(id)])
+    except IndexError:
+        return Response('Not Found', status=404)
+
+
+if __name__ == "__main__":
+    app.run()
